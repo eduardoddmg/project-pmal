4b369894997307c737f2c8cbcd1662df3ba34f44
eduardoddmg <eduardoddmg@gmail.com>
2023-10-01 10:52:55 -0300

    update pdf

--- src/pages/tco/[id].js
+++ src/pages/tco/[id].js
@@ -7,7 +7,99 @@ import { HeadComp, Loading, ModalImage } from "@/components";
 import { AiFillFilePdf } from "react-icons/ai";
 import { parseDateToEn } from "@/utils";
 import queryString from "query-string";
+import {
+  PDFDownloadLink,
+  Page,
+  Text,
+  Document,
+  View,
+  StyleSheet,
+} from "@react-pdf/renderer";
 
+const styles = StyleSheet.create({
+  page: {
+    flexDirection: "column",
+    backgroundColor: "#ffffff",
+  },
+  section: {
+    flexDirection: "column",
+    gap: 10,
+    margin: 10,
+    padding: 10,
+  },
+  heading: {
+    fontWeight: "bold",
+    backgroundColor: "#f0f0f0",
+    padding: 5,
+  },
+  text: {
+    marginBottom: 10,
+  },
+  containerPrincipal: {
+    flexDirection: "row",
+  },
+  containerTextPrincipal: {
+    width: "50%",
+    padding: 20,
+    backgroundColor: "#E2E8F0",
+  },
+  containerTextSecondary: {
+    width: "50%",
+    padding: 20,
+    backgroundColor: "#EDF2F7",
+  },
+});
+
+const MyPdfDocument = ({ data }) => { 
+  const dataCards = [
+    {
+      heading: "Infração Penal",
+      accessor: "infracao_penal"
+    },
+     {
+      heading: "Cidade",
+      accessor: "city"
+    },
+     {
+      heading: "Bairro",
+      accessor: "bairro"
+    },
+     {
+      heading: "Delegacia",
+      accessor: "delegacia"
+    },
+     {
+      heading: "Distância (KM)",
+      accessor: "dist"
+    },
+     {
+      heading: "Número do processo",
+      accessor: "n_process"
+    },
+    {
+      heading: "Número do TCO",
+      accessor: "n_tco"
+    },
+  ]
+  return (
+  <Document>
+    <Page style={styles.page}>
+      <View style={styles.section}>
+        <View style={{ marginBottom: 40 }}>
+          <Text style={{ textAlign: "center" }}>TCO - {data?.n_tco}</Text>
+        </View>
+        {dataCards?.map(item => <View style={styles.containerPrincipal}>
+          <View style={styles.containerTextPrincipal}>
+            <Text>{item.heading}</Text>
+          </View>
+          <View style={styles.containerTextSecondary}>
+            <Text>{data?.[item.accessor]}</Text>
+          </View>
+        </View>)}
+      </View>
+    </Page>
+  </Document>
+)};
 
 const Card = ({ heading, text }) => {
   return (
@@ -40,6 +132,7 @@ const PageId = () => {
     setLoading(true);
 
     const result = await readOne("tco", router.query.id);
+    result.dist = result.dist.toFixed(1);
     setData(result);
 
     setLoading(false);
@@ -50,7 +143,6 @@ const PageId = () => {
   }, []);
 
   const edit = () => {
-
     data.date = parseDateToEn(data.date);
     data.id = router.query.id;
 
@@ -80,11 +172,23 @@ const PageId = () => {
         isOpen={modalSignature.isOpen}
         onClose={modalSignature.onClose}
       />
+      <PDFDownloadLink
+        document={<MyPdfDocument data={data} />}
+        fileName={"TCO " + data?.n_tco + ".pdf"}
+      >
+        <Chakra.Button
+          alignSelf="start"
+          colorScheme="red"
+          rightIcon={<AiFillFilePdf />}
+        >
+          PDF
+        </Chakra.Button>
+      </PDFDownloadLink>
       <Card heading="Infração penal" text={data?.infracao_penal} />
       <Card heading="Cidade" text={data?.city} />
       <Card heading="Bairro" text={data?.bairro} />
       <Card heading="Delegacia" text={data?.delegacia} />
-      <Card heading="Distância" text={data?.dist.toFixed(0) + " KM"} />
+      <Card heading="Distância" text={data?.dist + " KM"} />
       <Card heading="Número do processo" text={data?.n_process} />
       <Card heading="Número do TCO" text={data?.n_tco} />
       <Card heading="Apreensão de material" text={data?.apreensao_material} />
@@ -97,10 +201,13 @@ const PageId = () => {
         boxSize="200px"
         objectFit="cover"
         cursor="pointer"
-        src={data?.imgUrl || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStYIhir1P4o-L9RV-U9_Q1CAja0IfnFE5i2w&usqp=CAU"}
+        src={
+          data?.imgUrl ||
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStYIhir1P4o-L9RV-U9_Q1CAja0IfnFE5i2w&usqp=CAU"
+        }
         onClick={modalAutor.onOpen}
-        />
-        <Chakra.Text>Assinatura PM</Chakra.Text>
+      />
+      <Chakra.Text>Assinatura PM</Chakra.Text>
       <Chakra.Image
         objectFit="cover"
         cursor="pointer"
